7035a8b1c2be43d3a55b2201c6b5038b32efc028
Peter Wi <peter.h.wi96@gmail.com>
2022-04-07 17:51:03 -0400

    Contains trapezoid function that was taught in Meeting 10

--- Ciya-FinalProject/Ciya-FinalProject/ContentView.swift
+++ Ciya-FinalProject/Ciya-FinalProject/ContentView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-        Text("New Change")
-        Text("Commit")
-        Text("checking how to commit and backup")
+struct Trapezoid: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path ()
+        
+        print(rect.minX, rect.maxX)
+        print(rect.minY, rect.maxY)
+        
+        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0.75 * rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: 0.25 * rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        return path
     }
 }
+
+struct ContentView: View {
+    var body: some View {
+        Trapezoid()
+            .stroke(Color.black)
+            .padding(50)
+            
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
