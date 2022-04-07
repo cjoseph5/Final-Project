879f4bfe0f6fb9b426af62b59f70e9deda49dad0
Peter Wi <peter.h.wi96@gmail.com>
2022-04-07 18:00:33 -0400

    Starting to create final project

--- Ciya-FinalProject/Ciya-FinalProject/ContentView.swift
+++ Ciya-FinalProject/Ciya-FinalProject/ContentView.swift
@@ -7,30 +7,17 @@
 
 import SwiftUI
 
-struct Trapezoid: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path ()
-        
-        print(rect.minX, rect.maxX)
-        print(rect.minY, rect.maxY)
-        
-        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: 0.75 * rect.maxX, y: rect.minY))
-        path.addLine(to: CGPoint(x: 0.25 * rect.maxX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        return path
+struct ContentView: View {
+    var body: some View {
+        ScrollView(.vertical) {
+            VStack {
+                Text("Stocks")
+                Text("April 7")
+            }
+        }
     }
 }
 
-struct ContentView: View {
-    var body: some View {
-        Trapezoid()
-            .stroke(Color.black)
-            .padding(50)
-            
-    }
-}
 
 
 struct ContentView_Previews: PreviewProvider {
