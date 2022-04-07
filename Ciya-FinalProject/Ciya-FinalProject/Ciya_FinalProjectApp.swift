333f4f4fc513cb93d423e7f6b70619b8e463996d
Peter Wi <peter.h.wi96@gmail.com>
2022-04-07 17:15:50 -0400

    create a xcode file

---
+++ Ciya-FinalProject/Ciya-FinalProject/Ciya_FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ciya_FinalProjectApp.swift
+//  Ciya-FinalProject
+//
+//  Created by peter wi on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Ciya_FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
