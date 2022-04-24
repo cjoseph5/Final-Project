35c93d0391b82af5c1222b56c8f7cfb22c721a0a
Ciya Joseph <ciyajoseph05@gmail.com>
2022-04-24 16:14:25 -0400

    Finished project

--- Ciya Final Project/Ciya Final Project/ContentView.swift
+++ Ciya Final Project/Ciya Final Project/ContentView.swift
@@ -7,37 +7,136 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct StockCell: Identifiable {
+    var id = UUID()
+    
+    var abrv: String
+    var company: String
+    var price: String
+    var change: String
+    var graph: String
+}
+
+struct StockRow: View {
+    var stockCell: StockCell
+    
     var body: some View {
-        ZStack {
-                HStack (alignment: .top){
-                    Text("9:00 PM")
-                        .position(x: 60, y: 25)
-                        .ignoresSafeArea()
-                    Image(systemName: "wifi")
-                        .position(x: 160, y: -25)
+        HStack {
+                VStack(alignment: .leading) {
                     Spacer()
-                    Image(systemName: "battery.75")
-                        .frame(maxWidth: 100, maxHeight: 40)
-                        .ignoresSafeArea()
+                    Text(stockCell.abrv)
+                    Text(stockCell.company).font(.subheadline).foregroundColor(.gray)
+                        .frame(width: 100, height: 20, alignment: .center)
                 }
-            HStack {
-                VStack (alignment: .leading){
-                    Text("Stocks")
-                        .font(.system(size: 32, weight: .bold))
-                        .foregroundColor(.black)
-                    Text("April 24")
-                        .font(.system(size: 32, weight: .bold))
-                        .foregroundColor(.gray)
+                Spacer()
+            Image(stockCell.graph)
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 70)
+                Spacer()
+            VStack (alignment: .trailing){
+                Text(stockCell.price)
+                Text(stockCell.change)
+                    .frame(maxWidth: 70, maxHeight: 20)
+                    .background(stockCell.graph == "redgraph" ? Color.red: Color.green)
                 }
-                
-        .position(x: 70, y: 40)
+            }
+        }
+    }
+
+
+struct ContentView: View {
+    @State private var showingSheet = false
+    @State var searchText = ""
+    @Environment(\.colorScheme) var colorScheme
+    
+    let stockRowsList = [
+        StockCell(abrv: "SBUX", company: "Starbucks Corporation", price: "80.17", change: "90.83B", graph: "redgraph"),
+        StockCell(abrv: "CMG", company: "Chipotle Mexican Grill, Inc.", price: "1,612.58", change: "42.47B", graph: "redgraph"),
+        StockCell(abrv: "NFLX", company: "Netflix, Inc.", price: "226.19", change: "96.95B", graph: "redgraph"),
+        StockCell(abrv: "TMUS", company: "T-Mobile US, Inc.", price: "80.17", change: "165B", graph: "redgraph"),
+        StockCell(abrv: "AMZN", company: "Amazon.com, Inc.", price: "3,079.96", change: "1.5T", graph: "greengraph"),
+        StockCell(abrv: "Dow Jones", company: "Dow Jones Industrial Average", price: "80.17", change: "--", graph: "greengraph"),
+        StockCell(abrv: "CRWD", company: "CrowdStrike Holdings, Inc.", price: "35,160.79", change: "48.43B", graph: "redgraph"),
+        StockCell(abrv: "TSLA", company: "Tesla, Inc.", price: "1,008.78", change: "1.4T", graph: "greengraph"),
+        StockCell(abrv: "WMT", company: "Walmart Inc.", price: "87.96", change: "439.9B", graph: "redgraph"),
+        StockCell(abrv: "XOM", company: "Exxon Mobil Corporation", price: "87.96", change: "388.4B", graph: "redgraph")]
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading) {
+                VStack {
+                    titleBar()
+                        .frame(maxWidth: 150, minHeight: 50)
+                        .offset(y: 50)
+                }
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(Color("LightGray"))
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                        TextField("Search...", text: $searchText)
+                    }
+                    .foregroundColor(.gray)
+                    .padding(.leading, 13)
+                }
+                .frame(height: 40)
+                .cornerRadius(13)
+                .padding()
+                .offset(y: 40)
+                List { ForEach(stockRowsList) { stockCell in NavigationLink(destination: DetailView(stockCell: stockCell)) {StockRow (stockCell: stockCell)
+                            }
+                        }.textCase(nil)
+                        .listRowBackground(colorScheme == .dark ? Color.black: Color.white)
+                    }.listStyle(GroupedListStyle())
+                    .offset(y: 30)
+            }
+            .ignoresSafeArea()
         }
     }
 }
+    
+struct titleBar: View {
+    var title: String = "Stocks"
+    var date: String = "April 24"
+    
+    var body: some View {
+        HStack {
+            VStack (alignment: .leading){
+                Text(title)
+                    .font(.system(size: 30, weight: .heavy))
+                Text(date)
+                    .font(.system(size: 30, weight: .heavy))
+                    .foregroundColor(.gray)
+            }
+        }
+    }
 }
+
+struct DetailView: View {
+    var stockCell: StockCell
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(stockCell.abrv)
+                    .font(.title)
+                    .padding()
+                Text(stockCell.company)
+            }
+            Spacer()
+            Image(stockCell.graph)
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 350)
+            Spacer()
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        }
     }
-}
---
+++ Ciya Final Project/Ciya Final Project/StocksView.swift
@@ -0,0 +1,21 @@
+//
+//  StocksView.swift
+//  Ciya Final Project
+//
+//  Created by Ciya Joseph on 4/21/22.
+//
+
+import SwiftUI
+
+struct StocksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StocksView_Previews: PreviewProvider {
+    static var previews: some View {
+        StocksView()
+    }
+}
+
